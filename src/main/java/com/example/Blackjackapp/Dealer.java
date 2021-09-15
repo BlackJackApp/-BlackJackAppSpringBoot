92a1c57e31c365534f8d092b58ba650a30ba2869
arkaan27 <arkaankanunga@gmail.com>
2021-09-15 11:55:04 +0100

    Added Dealer Class

---
+++ src/main/java/com/example/Blackjackapp/Dealer.java
@@ -0,0 +1,16 @@
+package com.example.Blackjackapp;
+
+public class Dealer extends Player {
+
+
+    public Dealer(){
+        super("Dealer");
+    }
+
+
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+    }
+
+}
