f1895f63bcbda83ba6533b5562111c50ba2ac18d
Samuel King <snks081018@gmail.com>
2021-09-15 11:56:31 +0100

    about to start hand tests

--- src/test/java/com/example/Blackjackapp/DeckTest.java
+++ src/test/java/com/example/Blackjackapp/DeckTest.java
@@ -2,6 +2,7 @@ package com.example.Blackjackapp;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 public class DeckTest {
 
     @Test
---
+++ src/test/java/com/example/Blackjackapp/HandTest.java
@@ -0,0 +1,15 @@
+package com.example.Blackjackapp;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HandTest {
+    //@Test
+    //@DisplayName("add card to hand")
+    //public void addCard(){
+    //    Hand hand = new Hand();
+    //    Card card = new Card(Suit.SPADES,Value.ACE);
+    //    hand.addCard(card);
+    //    assertEquals(card,hand,"can add cards to hand");
+    //}
+}
