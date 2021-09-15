4b06402208f4f5ff130b579f68c80eda283512ef
Samuel King <snks081018@gmail.com>
2021-09-15 11:02:53 +0100

    no duplication in deck tests that would make even Toby's skin crawl

--- src/test/java/com/example/Blackjackapp/DeckTest.java
+++ src/test/java/com/example/Blackjackapp/DeckTest.java
@@ -6,9 +6,112 @@ public class DeckTest {
 
     @Test
     @DisplayName("Deck Size to be 52")
-    public void noDuplicateCard(){
+    public void deckSize(){
         Deck deck = new Deck();
         int max = 52;
         assertSame(max, deck.getDeck().size(), "This is to test a deck has 52 cards");
+    }
+
+    @Test
+    @DisplayName("52 unique cards")
+    public void noDuplicatedSuits() {
+        Deck deck = new Deck();
+
+        int spadesCounter = 0;
+        int heartsCounter = 0;
+        int clubsCounter = 0;
+        int diamondsCounter = 0;
+
+        for (Card card : deck.getDeck()) {
+            if (card.getSuit() == Suit.SPADES) {
+                spadesCounter++;
+            } else if (card.getSuit() == Suit.HEARTS) {
+                heartsCounter++;
+            } else if (card.getSuit() == Suit.CLUBS) {
+                clubsCounter++;
+            } else if (card.getSuit() == Suit.DIAMOND) {
+                diamondsCounter++;
+            }
+        }
+
+        assertEquals(13, spadesCounter, "correct number of spades");
+        assertEquals(13, clubsCounter, "correct number of clubs");
+        assertEquals(13, heartsCounter, "correct number of hearts");
+        assertEquals(13, diamondsCounter, "correct number of diamonds");
+    }
+
+    @Test
+    @DisplayName("52 unique cards")
+    public void noDuplicatedValues() {
+        Deck deck = new Deck();
+
+        int aceCounter = 0;
+        int twoCounter = 0;
+        int threeCounter = 0;
+        int fourCounter = 0;
+        int fiveCounter = 0;
+        int sixCounter = 0;
+        int sevenCounter = 0;
+        int eightCounter = 0;
+        int nineCounter = 0;
+        int tenCounter = 0;
+        int jackCounter = 0;
+        int queenCounter = 0;
+        int kingCounter = 0;
+
+        for(Card card: deck.getDeck()){
+            if(card.getValue() == Value.ACE){
+               aceCounter++;
+            }
+            else if(card.getValue() == Value.TWO){
+                twoCounter++;
+            }
+            else if(card.getValue() == Value.THREE){
+                threeCounter++;
+            }
+            else if(card.getValue() == Value.FOUR){
+                fourCounter++;
+            }
+            else if(card.getValue() == Value.FIVE){
+                fiveCounter++;
+            }
+            else if(card.getValue() == Value.SIX){
+                sixCounter++;
+            }
+            else if(card.getValue() == Value.SEVEN){
+                sevenCounter++;
+            }
+            else if(card.getValue() == Value.EIGHT){
+                eightCounter++;
+            }
+            else if(card.getValue() == Value.NINE){
+                nineCounter++;
+            }
+            else if(card.getValue() == Value.TEN){
+                tenCounter++;
+            }
+            else if(card.getValue() == Value.JACK){
+                jackCounter++;
+            }
+            else if(card.getValue() == Value.QUEEN){
+                queenCounter++;
+            }
+            else if(card.getValue() == Value.KING){
+                kingCounter++;
+            }
+        }
+        assertEquals(4, aceCounter,"correct number 1");
+        assertEquals(4, twoCounter,"correct number 2");
+        assertEquals(4, threeCounter,"correct number 3");
+        assertEquals(4, fourCounter,"correct number 4");
+        assertEquals(4, fiveCounter,"correct number 5");
+        assertEquals(4, sixCounter,"correct number 6");
+        assertEquals(4, sevenCounter,"correct number 7");
+        assertEquals(4, eightCounter,"correct number 8");
+        assertEquals(4, nineCounter,"correct number 9");
+        assertEquals(4, tenCounter,"correct number 10");
+        assertEquals(4, jackCounter,"correct number 11");
+        assertEquals(4, queenCounter,"correct number 12");
+        assertEquals(4, kingCounter,"correct number 13");
     }
 }
