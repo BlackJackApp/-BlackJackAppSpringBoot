d90eae753e15490d578310c1e44cc1c85b49b716
Samuel King <snks081018@gmail.com>
2021-09-20 14:58:52 +0100

    75% coverage on game class

--- src/test/java/com/example/Blackjackapp/GameTest.java
+++ src/test/java/com/example/Blackjackapp/GameTest.java
@@ -19,7 +19,7 @@ public class GameTest {
     @Test
     @DisplayName("dealing cards to players")
     void dealingCards(){
-        Game game = new Game(2);
+        Game game = new Game(1);
         game.deal(2);
         ArrayList<Player> players = game.getPlayers();
         Player player = players.get(0);
@@ -28,5 +28,36 @@ public class GameTest {
         int handSize = hands.size();
         int expected = 2;
         assertEquals(expected,handSize,"can deal cards");
+    }
+
+    @Test
+    @DisplayName("player joins half way through")
+    void addPlayer(){
+        Game game = new Game(2);
+        ArrayList<Player> originalPlayers = game.getPlayers();
+        Player newPlayer = new Player();
+        ArrayList<Player> newPlayers = originalPlayers;
+        newPlayers.add(newPlayer);
+        game.setPlayers(newPlayers);
+        int expected = 3;
+        ArrayList<Player> actualValue = game.getPlayers();
+        int actual = actualValue.size();
+        assertEquals(expected,actual,"Can join mid-game");
+    }
+
+    @Test
+    @DisplayName("Display test")
+    void stringGeneratorTest(){
+        Game game = new Game(1);
+        String actual = game.gameToString();
+        String expected = "Dealer In your hand you have Hand 0" + System.lineSeparator() +
+                "You are currently scoring: [0]" + System.lineSeparator();
+        assertEquals(expected,actual,"String builder works");
+    }
+
+    @Test
+    @DisplayName("Shuffle getters and setters")
+    void forbiddenTest(){
+        
     }
 }
