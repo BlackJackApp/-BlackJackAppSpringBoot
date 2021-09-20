6db680282a341c07c185a9f303bca4a77ffa79f8
arkaan27 <arkaankanunga@gmail.com>
2021-09-20 10:27:53 +0100

    removed player test

--- src/test/java/com/example/Blackjackapp/PersonTest.java
+++ src/test/java/com/example/Blackjackapp/PersonTest.java
@@ -5,11 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PersonTest {
 
-<<<<<<< HEAD
     @Test
     @DisplayName("Initialisation")
     private void dealerInit(){
     }
-=======
->>>>>>> ff00adbd4ae05dc430f00b38b507afa58a843206
 }
